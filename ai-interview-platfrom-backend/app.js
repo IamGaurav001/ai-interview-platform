f6dd74dde1f2ad1935cffb7eb85565380f884ddc
IamGaurav001 <hellogauravdubey@gmail.com>
2025-10-24 08:06:54 +0530

    Backend and Gemini API Setup

--- ai-interview-platfrom-backend/app.js
+++ ai-interview-platfrom-backend/app.js
@@ -1,23 +1,22 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
-import db from "./config/db.js";
+import mongoose from "mongoose";
 
 dotenv.config();
-
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
+app.use(cors());
 app.use(express.json());
 
-
-
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.log("MongoDB error:", err.message));
 
 app.get("/", (req, res) => {
-  res.send("Hello, Express is running!");
+  res.send("AI Interview Coach Backend Running...");
 });
 
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
