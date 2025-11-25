7cf214ef165f3986138e754df43a54f484a050f3
IamGaurav001 <hellogauravdubey@gmail.com>
2025-11-25 22:28:29 +0530

    Minor Update

--- ai-interview-platform-frontend/src/config/firebase.js
+++ ai-interview-platform-frontend/src/config/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC6460IzM31wyatp3DZA7Fuosg-JspNo20",
-  authDomain: "ai-interview-coach-71c37.firebaseapp.com",
-  projectId: "ai-interview-coach-71c37",
-  storageBucket: "ai-interview-coach-71c37.firebasestorage.app",
-  messagingSenderId: "1092698828736",
-  appId: "1:1092698828736:web:f02c533c74fde05386e225",
-  measurementId: "G-JECDZYFPWE"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
